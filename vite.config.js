dc40b32acd3671796599bcf955f3f45d95ffd133
Srishti Sudhakar <166419213+sristtt@users.noreply.github.com>
2025-07-19 08:51:33 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base:"/Srishti_Portfolio/",
+    base: "/Srishti_Portfolio/",
+    plugins: [react()],
     build: {
         minify: "terser",
     },
-})
+});
